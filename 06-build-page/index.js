28a939994fe703f22d5f04c82072343912006feb
John Doe <2ivShr2!>
2024-01-23 01:00:20 +0100

    feat: implement remove template at 06-build-page

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -8,6 +8,7 @@ const styles = path.join(__dirname, 'styles');
 const assetsFolder = path.join(__dirname, 'assets');
 
 const outputFolder = path.join(__dirname, 'project-dist');
+const outputIndexPath = path.join(outputFolder, 'index.html');
 
 const outputStyleFile = path.join(outputFolder, 'style.css');
 
@@ -17,15 +18,24 @@ const templateContent = fs.readFileSync(templatePath, 'utf-8');
 
 
 const createNewDir = () => {
+    console.log('Starting the operation...');
+    console.log('Creating new directory...');
     fs.mkdirSync(outputFolder, { recursive: true });
-    removeHTMLTemplates(templateContent, componentsFolder);
+    console.log('Directory created!');
+
+    const updatedContent = removeHTMLTemplates(templateContent, componentsFolder);
+    fs.writeFileSync(outputIndexPath, updatedContent, 'utf-8');
     buildCssBundle();
+
+    
 };
 
 const removeHTMLTemplates = (content, componentsFolder) => {
+    console.log('Removing HTML templates...');
+
     let result = content;
     let startIndex = result.indexOf('{{');
-    
+
     while (startIndex !== -1) {
         const endIndex = result.indexOf('}}', startIndex + 2);
 
@@ -33,21 +43,26 @@ const removeHTMLTemplates = (content, componentsFolder) => {
             const tag = result.substring(startIndex + 2, endIndex);
             const componentFilePath = path.join(componentsFolder, `${tag}.html`);
 
-            fs.existsSync(componentFilePath)
-            const componentContent = fs.readFileSync(componentFilePath, 'utf-8');
-            result = result.slice(0, startIndex) + componentContent + result.slice(endIndex + 2);
+            if (fs.existsSync(componentFilePath)) {
+                const componentContent = fs.readFileSync(componentFilePath, 'utf-8');
+                result = result.slice(0, startIndex) + componentContent + result.slice(endIndex + 2);
+            } else {
+                console.warn(`Component file not found: ${componentFilePath}`);
+            }
         } else {
-            break; 
+            break;
         }
-
         startIndex = result.indexOf('{{', endIndex + 2);
     }
 
+    console.log('HTML Templates removed!');
     return result;
-}
+};
 
 
 const buildCssBundle = () => {
+    console.log('Creating styles bundle...');
+    
     const stylesArray = [];
      const files = fs.readdirSync(styles);
 
@@ -62,6 +77,7 @@ const buildCssBundle = () => {
     const cont = stylesArray.join('\n');
     fs.writeFileSync(outputStyleFile, cont, 'utf-8');
 
-    console.log('Styles bundled successfully!');
-
+    console.log('Styles bundle created!');
 };
+
+createNewDir();
