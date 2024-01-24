17408ac7cd852843b9146cfd911626d6cc0947af
John Doe <2ivShr2!>
2024-01-24 11:22:12 +0100

    refactor: update 04-copy-directory without sync -.-

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,20 +1,26 @@
-const fs = require('fs');
+const fs = require('fs').promises;;
 const path = require('path');
 
 const pathFolder = path.join(__dirname, 'files');
 const folderCopyDirrectory = path.join(__dirname, 'files-copy');
 
 
-const copyDir = async() => {
-    fs.rm(folderCopyDirrectory, { recursive: true, force: true });
-    fs.mkdir(folderCopyDirrectory, { recursive: true });
-    const files = fs.readdirSync(pathFolder);
-    files.forEach(file => {
-        const sourcePath = path.join(pathFolder, file);
-        const targetPath = path.join(folderCopyDirrectory, file);
-        const stats = fs.statSync(sourcePath);
-        fs.copyFileSync(sourcePath, targetPath);
-    });
+const copyDir = async () => {
+    try {
+        await fs.rm(folderCopyDirrectory, { recursive: true, force: true });
+        await fs.mkdir(folderCopyDirrectory, { recursive: true });
+        const files = await fs.readdir(pathFolder);
+        await Promise.all(files.map(async (file) => {
+            const sourcePath = path.join(pathFolder, file);
+            const targetPath = path.join(folderCopyDirrectory, file);
+
+            const stats = await fs.stat(sourcePath);
+            await fs.copyFile(sourcePath, targetPath);
+        }));
+    } catch (err) {
+        console.error('error', err);
+    }
 };
+
 
 copyDir();
